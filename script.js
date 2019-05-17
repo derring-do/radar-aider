b8a58263d5bbe7a758676a326c5586f2ae2f6c73
unknown <5824530+derring-do@users.noreply.github.com>
2019-05-16 23:25:10 -0400

    random spacing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var myCanvas = document.getElementById('chart-0');
 var ctx = document.getElementById('chart-0').getContext('2d');
 
 // Populate input fields with default data
-for(names = [], i=0; i<data.datasets.length; i++) {
+for (names = [], i = 0; i < data.datasets.length; i++) {
   names.push(data.datasets[i].label)
 }
 
@@ -11,11 +11,11 @@ document.querySelector("#attributes").value = data.labels.join().replace(/,/g, "
 
 // Define pallette for names
 var palette = [
- " rgba(8, 7, 8, 1)",
-  "rgba(55, 114, 255, 1)",
-  "rgba(223, 41, 53, 1)",
-  "rgba(253, 202, 64, 1)",
-  "rgba(230, 232, 230, 1)"
+  " rgba(8, 7, 8, .3)",
+  "rgba(55, 114, 255, .3)",
+  "rgba(223, 41, 53, .3)",
+  "rgba(253, 202, 64, .3)",
+  "rgba(230, 232, 230, .3)"
 ];
 
 function updateConfigByMutating() {
@@ -26,7 +26,7 @@ function updateConfigByMutating() {
   myChart.data.labels = attributes2;
   myChart.data.datasets = datasets2;
   myChart.update();
-  }
+}
 
 var myChart = new Chart(ctx, {
   type: 'radar',
@@ -38,21 +38,21 @@ var myChart = new Chart(ctx, {
     },
     scale: {
       pointLabels: {
-        fontSize: 14 
+        fontSize: 14
       },
       ticks: {
-        max: 120, 
+        max: 120,
         min: 0,
         stepSize: 0
       },
       layout: {
         padding: {
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
         }
-    }
+      }
     },
 
     //tooltips: {
@@ -65,10 +65,10 @@ var myChart = new Chart(ctx, {
     //  }
     //},
 
-    dragData: true, 
+    dragData: true,
     dragX: true,
-    dragDataRound: 1, 
-    onDragEnd: function(event, datasetIndex, index, value) {
+    dragDataRound: 1,
+    onDragEnd: function (event, datasetIndex, index, value) {
       console.log(`datasetIndex:${datasetIndex}, index:${index}, value:${value}`);
       document.getElementById('item').innerHTML = myChart.config.data.labels[index];
       document.getElementById('value').innerHTML = value;
@@ -76,19 +76,17 @@ var myChart = new Chart(ctx, {
   }
 });
 
-// function makeData(names, attributes, datasets) {
-// for(i=0; i<names.length; i++) {
-//   var data = {
-//     label: names[i],
-//     data: Array(attributes.length).fill(myChart.options.scale.ticks.max - (i+.25)),
-//     backgroundColor: palette[i],
-//     pointRadius: 10,
-//     pointHoverRadius: 10
-//     };
-//     console.log(data)
-//     datasets.push(data)
-// };
-// }
-
-// makeData(names, attributes, datasets);
-
+function makeData(names, attributes, datasets) {
+  var max = myChart.options.scale.ticks.max;
+  for (i = 0; i < names.length; i++) {
+    var data = {
+      label: names[i],
+      data: Array(attributes.length).fill(max / (1 + i)),
+      backgroundColor: palette[i],
+      pointRadius: 5,
+      pointHoverRadius: 10
+    };
+    console.log(data)
+    datasets.push(data)
+  };
+}
